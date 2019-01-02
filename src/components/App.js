3d9b11285496f8cf0a18ef6260fdb9a0671df795
Victor <viktor.markevich@faceit.com.ua>
2019-01-02 14:54:09 +0200

    Cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './Home';
 import AuthenticatedComponent from "./AuthenticatedComponent";
 import Login from "./Login";
-// import Protected from "./Protected";
 import Dashboard from "./Dashboard";
 
 class App extends Component {
@@ -12,9 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
             <Route path="/login" component={Login} />
-            <Route path="/" exact component={Home} />
             <AuthenticatedComponent>
-              {/*<Route path="/protected" exact component={Protected} />*/}
               <Route path="/dashboard" exact component={Dashboard} />
             </AuthenticatedComponent>
         </Switch>
--- src/components/Protected.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Protected = () => {
-    return (
-        <h1>I'm protected!</h1>
-    )
-};
-
-export default Protected;
